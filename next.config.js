07ab4f3bbf4e4272a8dfada918ea262e4f565417
DerickLacerda <derick.rl2004@gmail.com>
2025-03-21 01:55:31 -0300

    vercel

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: [
+    "lucide-react",
+    "@radix-ui/react-avatar",
+    "@radix-ui/react-dialog",
+    "@radix-ui/react-label",
+    "@radix-ui/react-slot",
+  ],
 }
 
 module.exports = nextConfig
